b432edac55e6cae3da40dfa1b76bf787bb6cf4db
DESKTOP-I598EL5\Dell <rabiagull.bektas@gmail.com>
2022-05-22 11:08:20 +0300

    C# entegre

---
+++ githubr/githubr/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
